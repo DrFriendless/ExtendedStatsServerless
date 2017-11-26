c90c95e533fc43bb5b579dfbc351c0e44771a738
DrFriendless <Borod1n>
2017-11-27 00:35:04 +1100

    next bug is not authorised to read config from S3

--- users-to-mongo/mongo.ts
+++ users-to-mongo/mongo.ts
@@ -5,5 +5,13 @@ import {StitchClient} from "mongodb-stitch";
 
 export const ensureUser: (config: Configuration, user: string) => void = (config: Configuration, user: string) => {
     const client = new StitchClient(config["mongoStitchID"]);
-    console.log("mongo " + user);
+    client.login(config["mongoLogin"], config["mongoPwd"])
+        .then(() => console.log('logged in as: ' + client.authedId()))
+        .catch(e => console.log('error: ', e));
+    const db = client.service('mongodb', 'mongodb1').db('Extended');
+    const usersCollection = db.collection('users');
+    let userId = client.authedId();
+    usersCollection.insertMany([ { owner_id: userId, username: user } ])
+        .then(result => console.log('success: ', result))
+        .catch(e => console.log('error: ', e));
 };
