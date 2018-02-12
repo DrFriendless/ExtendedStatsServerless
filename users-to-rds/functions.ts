cc2c957b02b9c246d7a2227fa03f3580f562b94a
DrFriendless <friendless.farrell@gmail.com>
2018-02-12 21:17:00 +1100

    working on list users

--- users-to-rds/functions.ts
+++ users-to-rds/functions.ts
@@ -1,5 +1,5 @@
 import {Callback, Handler} from 'aws-lambda';
-import {ensureUsers} from "./mysql-rds";
+import {ensureUsers, listUsers} from "./mysql-rds";
 
 // Lambda to get the list of users from an SQS queue and write it to Mongo DB.
 export const writeToDB: Handler = (event, context, callback: Callback) => {
@@ -7,9 +7,13 @@ export const writeToDB: Handler = (event, context, callback: Callback) => {
     // const SnsPublishTime = event.Records[0].Sns.Timestamp;
     // const SnsTopicArn = event.Records[0].Sns.TopicArn;
     const body = event.Records[0].Sns.Message;
-    console.log(body);
     const usernames = body.split(/\r?\n/);
     console.log("checking for " + usernames.length + " users");
     ensureUsers(usernames);
 };
 
+export const userlist: Handler = (event, context, callback: Callback) => {
+    console.log("userlists");
+    context.callbackWaitsForEmptyEventLoop = false;
+    listUsers(callback);
+};
--- users-to-rds/mysql-rds.ts
+++ users-to-rds/mysql-rds.ts
@@ -1,4 +1,7 @@
 import mysql = require('mysql');
+import {Callback} from "aws-lambda";
+
+const PROFILE_URL = "https://boardgamegeek.com/user/";
 
 export const ensureUsers: (users: string[]) => void = (users: string[]) => {
     const conn = getConnection();
@@ -20,13 +23,59 @@ function doEnsureUsers(conn: mysql.Connection, users: string[]) {
                 if (count === 0) {
                     conn.query(insertSql, [user]);
                 }
+                doEnsureFileProcessUser(conn, user);
             });
         });
     });
 }
 
+function doRecordFile(conn: mysql.Connection, url: string, processMethod: string, user: string, description: string) {
+    const countSql = "select count(*) from files where geek = ? and processMethod = ?";
+    const insertSql = "insert into files (url, processMethod, geek, lastupdate, tillNextUpdate, description) values (?, ?, ?, ?, ?, ?)";
+    conn.query(countSql, [user, processMethod], (err, result) => {
+        if (err) throw err;
+        const count = result[0]["count(*)"];
+        if (count === 0) {
+            const tillNext = TILL_NEXT_UPDATE[processMethod];
+            const insertParams = [url, processMethod, user, null, tillNext, description];
+            conn.query(insertSql, insertParams);
+        }
+    });
+}
+
+// longest possible MySQL time is 838:59:59 hours: http://dev.mysql.com/doc/refman/5.5/en/date-and-time-type-overview.html
+const TILL_NEXT_UPDATE = { 'processCollection' : '72:00:00', 'processMarket' : '72:00:00', 'processPlayed' : '72:00:00',
+        'processGame' : '838:00:00', 'processTop50' : '72:00:00', "processFrontPage" : '24:00:00' };
+
+function doEnsureFileProcessUser(conn: mysql.Connection, user: string) {
+    console.log("doEnsureFileProcessUser");
+    doRecordFile(conn, PROFILE_URL + user, "processUser", user, "User's profile");
+}
+
+export const listUsers: (callback: Callback) => void = (callback: Callback) => {
+    console.log("listUsers");
+    const conn = getConnection();
+    conn.connect(err => {
+        if (err) {
+            callback(err);
+            return;
+        }
+        console.log("listUsers connected");
+        const sql = "select username from geeks";
+        conn.query(sql, null, (err, result) => {
+            if (err) {
+                callback(err);
+                return;
+            }
+            const val = result.map(row => row['username']);
+            console.log(val);
+            callback(null, val);
+            console.log("listUsers complete");
+        });
+    });
+};
+
 function getConnection(): mysql.Connection {
-    console.log("getConnection");
     const params = {
         host: process.env.mysqlHost,
         user: process.env.mysqlUsername,
