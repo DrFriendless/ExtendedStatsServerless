61c0d5cb76f60232bff0f7d1fd757be747866b3a
DrFriendless <friendless.farrell@gmail.com>
2018-07-21 16:00:15 +1000

    move www over to mustache

---
+++ www/package.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+#cp src/*.css static
+cp src/*.txt static
+for F in src/*.mustache
+    do
+    FILENAME=`basename ${F}`
+    FILENAME=${FILENAME%%.*}
+    mustache values.json -p src/partials/adsbygoogle.mustache -p src/partials/gtmbody.mustache -p src/partials/gtmhead.mustache ${F} >static/${FILENAME}.html
+    done
+
