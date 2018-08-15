f6cc5848371dfebb6dfc2ad254295a5efd6d5b22
DrFriendless <friendless.farrell@gmail.com>
2018-08-15 20:32:07 +1000

    created extstats-core

---
+++ jslib/corelib/projects/extstats-core/src/lib/admin-interfaces.ts
@@ -0,0 +1,23 @@
+export interface TypeCount {
+   type: string;
+   existing: number;
+   waiting: number;
+   unprocessed: number;
+}
+
+export interface SystemStats {
+    userRows: number;
+    gameRows: number;
+    geekGamesRows: number;
+    expansionRows: number;
+    mechanics: number;
+    categories: number;
+    gameMechanics: number;
+    gameCategories: number;
+    notGames: number;
+    fileRows: TypeCount[];
+    ggForZero: number;
+    distinctGGOwners: number;
+    playsRows: number;
+    normalisedPlaysRows: number;
+}
---
+++ jslib/corelib/projects/extstats-core/src/public_api.ts
@@ -0,0 +1,9 @@
+/*
+ * Public API Surface of extstats-core
+ */
+
+export * from './lib/admin-interfaces';
+export * from './lib/collection-interfaces';
+export * from './lib/expansion-data';
+export * from './lib/ranking-interfaces';
+export * from './lib/security-interfaces';
