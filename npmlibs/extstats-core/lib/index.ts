a1e227da354973c577c315becb9a5496f24754be
DrFriendless <friendless.farrell@gmail.com>
2018-08-18 09:30:05 +1000

    second attempt at core library

---
+++ npmlibs/extstats-core/lib/index.ts
@@ -0,0 +1,11 @@
+/*
+ * Public API Surface of extstats-core
+ */
+
+export * from './admin-interfaces';
+export * from './collection-interfaces';
+export * from './expansion-data';
+export * from './extstats-storage';
+export * from './favourites-interfaces';
+export * from './ranking-interfaces';
+export * from './security-interfaces';
---
+++ npmlibs/extstats-core/lib/ranking-interfaces.ts
@@ -0,0 +1,11 @@
+export interface RankingTableRow {
+    game: number;
+    game_name: string;
+    total_ratings: number;
+    num_ratings: number;
+    bgg_ranking: number;
+    bgg_rating: number;
+    normalised_ranking: number;
+    total_plays: number;
+    ranking: number;
+}
