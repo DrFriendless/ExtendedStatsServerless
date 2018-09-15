1c74b0651d1813a4490800b7cf037e5951700227
DrFriendless <friendless.farrell@gmail.com>
2018-09-15 22:18:18 +1000

    export DataViewComponent and DataSourceComponent

--- npmlibs/extstats-angular/projects/extstats-angular/src/public_api.ts
+++ npmlibs/extstats-angular/projects/extstats-angular/src/public_api.ts
@@ -7,3 +7,5 @@ export * from './lib/charts';
 export * from "./lib/button-group/button-group-interfaces";
 export * from "./lib/extstats-documentation/documentation";
 export * from "./lib/table-config/extstats-table";
+export * from './lib/data-source-component';
+export * from './lib/data-view-component';
