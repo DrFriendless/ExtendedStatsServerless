02b40e8c219f5b043e55e016f7ed22834e5c2e8f
DrFriendless <friendless.farrell@gmail.com>
2018-09-15 22:06:11 +1000

    extra DataSourceComponent

--- client/ownedCollection/src/app/app.component.ts
+++ client/ownedCollection/src/app/app.component.ts
@@ -1,61 +1,32 @@
-import { Component, AfterViewInit, OnDestroy } from '@angular/core';
-import {HttpClient, HttpHeaders} from "@angular/common/http";
-import { Collection, fromExtStatsStorage, GeekGameQuery } from "extstats-core";
-import { ExtstatsTable } from "extstats-angular";
-import {Observable} from "rxjs/internal/Observable";
-import {Subscription} from "rxjs/internal/Subscription";
-import {Subject} from "rxjs/internal/Subject";
-import {flatMap, tap, map, share} from "rxjs/internal/operators";
+import { Component } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import { Collection } from "extstats-core";
+import {DataSourceComponent} from "./data-source-component";
 
 @Component({
   selector: 'owned-collection',
   templateUrl: './app.component.html'
 })
-export class UserOwnedComponent implements ExtstatsTable, AfterViewInit {
+export class UserOwnedComponent extends DataSourceComponent<Collection> {
   private static DEFAULT_SELECTOR = "owned(ME)";
-  public data$: Observable<Collection>;
-  private selectors = new Subject<string>();
-  private selector: string = UserOwnedComponent.DEFAULT_SELECTOR;
-  private geek: string;
 
-  constructor(private http: HttpClient) {
-    this.geek = fromExtStatsStorage(storage => storage.geek);
-    this.data$ = this.selectors.asObservable()
-      .pipe(
-        flatMap(s => this.doQuery(s)),
-        tap(data => console.log(data)),
-        share()
-      );
-  }
-
-  public ngAfterViewInit() {
-    this.selectors.next(this.selector);
+  constructor(http: HttpClient) {
+    super(http, UserOwnedComponent.DEFAULT_SELECTOR);
   }
 
   public getId(): string {
     return "rated";
   }
 
-  public getSelector(): string {
-    return this.selector;
+  protected getQueryResultFormat(): string {
+    return "Collection";
   }
 
-  public setSelector(s: string) {
-    this.selector = s;
-    this.selectors.next(s);
+  protected getQueryVariables(): { [p: string]: string } {
+    return {};
   }
 
-  private doQuery(selector: string): Observable<Collection> {
-    const options = {
-      headers: new HttpHeaders().set("x-api-key", "gb0l7zXSq47Aks7YHnGeEafZbIzgmGBv5FouoRjJ")
-    };
-    const body: GeekGameQuery = {
-      query: selector,
-      geek: this.geek,
-      format: "Collection",
-      vars: {}
-    };
-    console.log(body);
-    return this.http.post("https://api.drfriendless.com/v1/query", body, options) as Observable<Collection>;
+  protected getApiKey(): string {
+    return "gb0l7zXSq47Aks7YHnGeEafZbIzgmGBv5FouoRjJ";
   }
 }
