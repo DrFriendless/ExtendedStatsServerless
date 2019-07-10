14b61692ef7eac05386446d296cd475654ede219
DrFriendless <friendless.farrell@gmail.com>
2019-07-11 07:53:09 +1000

    graphQL retrieve plays: got the first, startYMD, endYMD args working, general clean-up

--- api/retrieve.ts
+++ api/retrieve.ts
@@ -6,28 +6,24 @@ import { MultiGeekPlays } from "extstats-core";
 import { doRetrieveGames } from "./mysql-rds";
 import { asyncReturnWithConnection, getGeekIds } from "./library";
 
+const ListOfString = new graphql.GraphQLList(graphql.GraphQLString!);
+const ListOfInt = new graphql.GraphQLList(graphql.GraphQLInt!);
+
 const schema = new graphql.GraphQLSchema({
   query: new graphql.GraphQLObjectType({
     name: "RetrieveQuery",
     fields: {
-      // the query has a field called 'greeting'
-      greeting: {
-        // we need to know the user's name to greet them
-        args: { firstName: { name: "firstName", type: new graphql.GraphQLNonNull(graphql.GraphQLString) } },
-        // the greeting message is a string
-        type: graphql.GraphQLString,
-        resolve: async (parent, args) => await getGreeting(args.firstName)
-      },
-      getPlays: {
+      plays: {
         args: {
-          geeks: { name: "geeks", type: new graphql.GraphQLList(graphql.GraphQLString)! },
+          geeks: { name: "geeks", type: ListOfString },
+          first: { name: "first", type: graphql.GraphQLBoolean },
           startYMD: { name: "startYMD", type: graphql.GraphQLInt },
           endYMD: { name: "endYMD", type: graphql.GraphQLInt }
         },
         type: new GraphQLObjectType({
           name: "MultiGeekPlays",
           fields: {
-            geeks: { type: new graphql.GraphQLList(graphql.GraphQLString)! },
+            geeks: { type: ListOfString },
             plays: {
               type: new graphql.GraphQLList(new graphql.GraphQLObjectType({
                 name: "PlaysWithDate",
@@ -38,7 +34,7 @@ const schema = new graphql.GraphQLSchema({
                   day: { type: graphql.GraphQLInt! },
                   ymd: { type: graphql.GraphQLInt! },
                   game: { type: graphql.GraphQLInt! },
-                  expansions: { type: new graphql.GraphQLList(graphql.GraphQLInt) },
+                  expansions: { type: ListOfInt },
                   quantity: { type: graphql.GraphQLInt! }
                 }
               }))!
@@ -65,19 +61,25 @@ const schema = new graphql.GraphQLSchema({
         }),
         resolve: async (parent, args) =>
           await asyncReturnWithConnection(async conn =>
-            await playsQueryForRetrieve(conn, args.geeks, true)
+            await playsQueryForRetrieve(conn, args.geeks, !!args.first, args.startYMD || 0, args.endYMD || 30000000)
           )
       }
     }
   })
 });
 
+type RetrievePlay = {
+  game: number;
+  quantity: number;
+  ymd: number;
+  year: number;
+  month: number;
+  date: number;
+  geek: string;
+  expansions: number[];
+};
 
-export async function getGreeting(firstName: string): Promise<string> {
-  return `Hello ${firstName}`;
-}
-
-async function playsQueryForRetrieve(conn: mysql.Connection, geeks: string[], first: boolean) {
+async function playsQueryForRetrieve(conn: mysql.Connection, geeks: string[], first: boolean, startInc: number, endExc: number) {
   const geekNameIds: { [id: number]: string } = await getGeekIds(conn, geeks);
   if (Object.values(geekNameIds).length === 0) {
     return { geeks: [], plays: {}, collection: [], games: [] };
@@ -91,47 +93,48 @@ async function playsQueryForRetrieve(conn: mysql.Connection, geeks: string[], fi
   if (first) where += " order by ymd asc";
   const playsSql = "select (year * 10000 + month * 100 + date) ymd, id, game, geek, quantity, year, month, date, expansion_play, baseplay from plays_normalised where " + where;
   const playsResult = await conn.query(playsSql, args);
-  const expPlays = [];
-  const basePlays: object[] = [];
-  const playsById = {};
-  const gameIds: number[] = [];
+  const expPlays: object[] = [];
+  const basePlays: RetrievePlay[] = [];
+  const playsById: Record<number, RetrievePlay> = {};
   const firstKeys: string[] = [];
   for (const row of playsResult) {
-    if (gameIds.indexOf(row.game) < 0) gameIds.push(row.game);
     if (first) {
       const firstKey = `${row.game}-${row.geek}`;
       if (firstKeys.indexOf(firstKey) >= 0) continue;
       firstKeys.push(firstKey);
     }
+    if (row.ymd < startInc || row.ymd >= endExc) continue;
     if (row["expansion_play"]) {
       expPlays.push(row);
     } else {
-      const pwd: object = { game: row.game, quantity: row.quantity };
-      pwd['ymd'] = row.ymd;
-      pwd['year'] = row.year;
-      pwd['month'] = row.month;
-      pwd['date'] = row.date;
       const username = geekNameIds[row.geek];
       if (!username) {
         console.log("No user found for " + row.geek);
         continue;
       }
-      pwd['geek'] = username;
+      const pwd: RetrievePlay = { game: row.game, quantity: row.quantity, ymd: row.ymd, year: row.year, month: row.month, date: row.date,
+        geek: username, expansions: [] };
       basePlays.push(pwd);
       playsById[row.id] = pwd;
     }
   }
   for (const ep of expPlays) {
-    const pwd = playsById[ep.baseplay];
+    const pwd = playsById[ep["baseplay"]];
     if (!pwd) {
-      console.log("No base play " + ep.baseplay + " found");
+      console.log("No base play " + ep["baseplay"] + " found");
       continue;
     }
-    if (!pwd.expansions) pwd.expansions = [];
-    pwd.expansions.push(ep.game);
+    pwd.expansions.push(ep["game"]);
+  }
+  const gameIds: number[] = [];
+  for (const p of basePlays) {
+    if (gameIds.indexOf(p.game) < 0) gameIds.push(p.game);
+    for (const e of p.expansions) {
+      if (gameIds.indexOf(e) < 0) gameIds.push(e);
+    }
   }
   const games = await doRetrieveGames(conn, gameIds);
-  return { geeks: Object.values(geekNameIds), plays: basePlays, collection: [], games };
+  return { geeks: Object.values(geekNameIds), plays: basePlays, games };
 }
 
 export async function retrieve(event: APIGatewayProxyEvent, context: Context, callback: Callback) {
