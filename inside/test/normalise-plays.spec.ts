6d6fcece03c3eff37cf18c8b7e7a517e51be65e8
DrFriendless <Borod1n>
2020-06-06 21:57:23 +1000

    actual test that does a thing

--- inside/test/normalise-plays.spec.ts
+++ inside/test/normalise-plays.spec.ts
@@ -3,21 +3,21 @@ import {WorkingNormalisedPlays} from "../src/interfaces";
 import * as bob from "../src/plays";
 
 const ONE_PLAY_OF_GAME = {
-    expansions: [], isExpansion: false, quantity: 1, game: 1, geek: 1, date: 1, month: 1, year: 2020
+    expansions: [], isExpansion: false, quantity: 1, game: 320, geek: 1, date: 1, month: 1, year: 2020
 };
 const TWO_PLAYS_OF_GAME = {
-    expansions: [], isExpansion: false, quantity: 2, game: 1, geek: 1, date: 1, month: 1, year: 2020
+    expansions: [], isExpansion: false, quantity: 2, game: 320, geek: 1, date: 1, month: 1, year: 2020
 };
 
 test('bob', () => {
     console.log(bob);
-    console.log(sumQuantities);
 });
 
 describe('working normalised plays', () => {
     it('should sum quantities', () => {
         const src: WorkingNormalisedPlays[] = [ ONE_PLAY_OF_GAME, TWO_PLAYS_OF_GAME ];
         const summed = sumQuantities(src);
-        console.log(summed);
+        expect(summed.quantity).toBe(3);
+        expect(summed.game).toBe(320);
     })
 });
